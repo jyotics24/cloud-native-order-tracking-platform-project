3f0d042188923e4271924e7036eef27d84f594bd
JYOTI <jyotiprakashkhuntia999@gmail.com>
2026-06-23 22:18:53 +0530

    SonarCloud security test

--- app/backend/app.py
+++ app/backend/app.py
@@ -2,6 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
+password = "admin123"
+
 orders = []
 
 @app.route("/health")
@@ -25,4 +27,4 @@ def get_order(order_id):
     return {"error": "Order not found"}, 404
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000
+    app.run(host="0.0.0.0", port=5000)
